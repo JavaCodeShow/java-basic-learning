73aae04662470c1f0ccb7a4b575c8dd6b047a675
江峰 <jiangfeng@morefungroup.com>
2022-11-01 16:33:54 +0800

    fix

---
+++ src/main/java/com/jf/domain/User.java
@@ -0,0 +1,26 @@
+package com.jf.domain;
+
+/**
+ * @author 江峰
+ * @date 2022/8/3 14:32
+ */
+public class User {
+    String id;
+    String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
