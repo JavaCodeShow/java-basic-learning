cf194a361c090c9abb45c1aa68151948761faa0e
江峰 <jiangfeng@morefungroup.com>
2024-03-21 15:11:42 +0800

    fix

--- src/main/java/com/jf/Main.java
+++ src/main/java/com/jf/Main.java
@@ -3,6 +3,7 @@ package com.jf;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("hello1");
+        // System.out.println(Runtime.getRuntime().availableProcessors());
     }
 }
 
